f5713453dd9b3bb1eddefce3ed901f274fcbb67b
Hiruy Legesse <hiruyadane@gmail.com>
2026-02-05 23:02:06 +0300

    Add duplicate in array file

---
+++ duplicatesInArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        result= []
+        for i in count:
+            if count[i] > 1:
+                result.append(i)
+        return result
